ec65ad813d700c33d78e365345244582b5335749
zuhwan0205 <ljh20230379@gmail.com>
2025-03-05 09:19:47 +0900

    Day008

---
+++ Day009/Day009/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] names = { "Charlie", "Alice", "Bob" };
+var sortedNames = names.OrderBy(n => n);
+
+foreach(var name in sortedNames)
+{
+    Console.WriteLine(name);
+}
+
+var firstName = names.First(n => n.StartsWith("A"));
+
+Console.WriteLine($"First name starting with A: {firstName}");
